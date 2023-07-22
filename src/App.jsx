d765b9e1d215e37858772aeb6ee51e79ea36a3c2
Joel Mendez <joel.j.mendez.m@gmail.com>
2023-07-22 17:02:29 -0300

    Commits: *Add commented BookSearch component *Create BookSearch component *Change url fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import './App.css';
-import Books from './components/Books/Books';
-import Header from './components/Header/Header';
-import SelectedBooks from './components/SelectedBooks/SelectedBooks';
-import { Filter } from './components/filter/Filter';
-import { SelectedBookProvider } from './context/SelectedBookProvider';
+import "./App.css";
+// import BookSearch from "./components/Books/BookSearch/BookSearch";
+import Books from "./components/Books/Books";
+import Header from "./components/Header/Header";
+import SelectedBooks from "./components/SelectedBooks/SelectedBooks";
+import { Filter } from "./components/filter/Filter";
+import { SelectedBookProvider } from "./context/SelectedBookProvider";
 
 function App() {
   return (
     <SelectedBookProvider>
       <Header />
       <Filter />
+      {/* <BookSearch /> */}
       <Books />
       <section>
         <p>LIBROS SELECCIONADOS</p>
