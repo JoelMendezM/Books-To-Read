dad29bbc059997e9ad01e10dd5ed6ef428f014d1
Joel Mendez <joel.j.mendez.m@gmail.com>
2023-07-25 02:05:08 -0300

    Style and clean code

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
-import './App.css';
-import Books from './components/Books/Books';
-import Header from './components/Header/Header';
-import SelectedBooks from './components/SelectedBooks/SelectedBooks';
-import { Filter } from './components/filter/Filter';
-import { SelectedBookProvider } from './context/SelectedBookProvider';
+import { Flex } from "@chakra-ui/react";
+import "./App.css";
+import Books from "./components/Books/Books";
+import Header from "./components/Header/Header";
+import SelectedBooks from "./components/SelectedBooks/SelectedBooks";
+import { Filter } from "./components/filter/Filter";
+import { SelectedBookProvider } from "./context/SelectedBookProvider";
 
 function App() {
   return (
     <SelectedBookProvider>
       <Header />
       <Filter />
-      <Books />
-      <section>
-        <p>LIBROS SELECCIONADOS</p>
-        <SelectedBooks />
-      </section>
+      <Flex flexDirection="row">
+        <section>
+          <Books />
+        </section>
+        <section>
+          <SelectedBooks />
+        </section>
+      </Flex>
     </SelectedBookProvider>
   );
 }
--- src/components/SelectedBooks/SelectedBooks.jsx
+++ src/components/SelectedBooks/SelectedBooks.jsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem } from "@chakra-ui/react";
+import { Center, Container, Grid, GridItem, Text } from "@chakra-ui/react";
 import BookItem from "../Books/BookItem/BookItem";
 import { useBooks } from "../../context/SelectedBookProvider";
 
@@ -6,23 +6,32 @@ const SelectedBooks = () => {
   const { selectedBooks } = useBooks();
 
   return (
-    <Grid
-      templateColumns="repeat(4, 1fr)"
-      templateRows="repeat(2, 1fr)"
-      rowGap={1}
-      columnGap={5}
-    >
-      {selectedBooks.map((book) => (
-        <GridItem key={book.book.ISBN}>
-          <BookItem
-            key={book.book.ISBN}
-            id={book.book.ISBN}
-            cover={book.book.cover}
-            idx={"selectedBooks"}
-          />
-        </GridItem>
-      ))}
-    </Grid>
+    <Container bgColor="#3a3a3a" p={10} borderRadius={10} ml={10}>
+      {selectedBooks.length === 0 && (
+        <Center>
+          <Text color="white" fontSize={20}>
+            There is no pending books to read
+          </Text>
+        </Center>
+      )}
+      <Grid
+        templateColumns="repeat(2, 1fr)"
+        templateRows="repeat(2, 1fr)"
+        rowGap={1}
+        columnGap={5}
+      >
+        {selectedBooks.map((book) => (
+          <GridItem key={book.book.ISBN} m={2}>
+            <BookItem
+              key={book.book.ISBN}
+              id={book.book.ISBN}
+              cover={book.book.cover}
+              idx={"selectedBooks"}
+            />
+          </GridItem>
+        ))}
+      </Grid>
+    </Container>
   );
 };
 
